a74daa2ea6cad426318f114bb1d113939d22f3c2
g0at1 <michal.lendzion@vp.pl>
2023-08-14 19:45:02 +0200

    Exercise 1211. Queries Quality and Percentage (MYSQL)

---
+++ mysql/1211-queries-quality-and-percentage/solution.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT q.query_name, ROUND(AVG(q.rating / q.position), 2) AS quality, ROUND(AVG(q.rating < 3) * 100, 2) AS poor_query_percentage
+FROM Queries q
+GROUP BY q.query_name;
