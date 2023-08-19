16b624d4a13dd04e2a819929eed80c913406f415
g0at1 <michal.lendzion@vp.pl>
2023-08-19 17:50:50 +0200

    Exercise 28. Find the Index of the First Occurrence in a String (CPP)

---
+++ cpp/28-find-index-of-occurrence/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        std::size_t found = haystack.find(needle);
+        if (found != std::string::npos)
+            return found;
+        return -1;
+    }
+};
