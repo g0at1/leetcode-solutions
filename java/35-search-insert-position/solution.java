37df4cd3f1426cc74439af189f7978d2f7ffeab5
g0at1 <michal.lendzion@vp.pl>
2023-08-29 21:43:04 +0200

    Exercise 35. Search Insert Position (JAVA)

---
+++ java/35-search-insert-position/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return left;
+    }
+}
