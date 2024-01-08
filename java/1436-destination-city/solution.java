ad2e9adb1befa2cfe2dc365064ae94b035421c3d
g0at1 <michal.lendzion@vp.pl>
2024-01-08 12:15:22 +0100

    feat: Exercise 1436. Destination City

---
+++ java/1436-destination-city/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+       Set<String> cities = new HashSet<>();
+
+       for (List<String> path : paths) {
+           cities.add(path.get(0));
+       }
+
+       for (List<String> path : paths) {
+           String destination = path.get(1);
+           if (!cities.contains(destination)) {
+               return destination;
+           }
+       }
+
+       return "";
+    }
+}
