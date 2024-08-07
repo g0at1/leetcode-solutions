e0c1ba839a2e45af0fd2cda5aa1163c5d4d48908
g0at1 <michal.lendzion@vp.pl>
2024-08-07 19:52:12 +0200

    Feat: 2678. Number of senior citizens (CSHARP)

---
+++ csharp/2678-number-of-senior-citizens/solution.ts
@@ -0,0 +1,17 @@
+public class Solution 
+{
+    public int CountSeniors(string[] details) 
+    {
+        var result = 0;
+        foreach(var info in details)
+        {
+            var age = int.Parse(info.Substring(11, 2));
+            if (age > 60)
+            {
+                result++;
+            }
+        }
+        
+        return result;
+    }
+}
