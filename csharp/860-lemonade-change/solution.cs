2d6440913436e8c818662f04b85acba6ed676ddf
g0at1 <michal.lendzion@vp.pl>
2024-08-15 19:01:28 +0200

    Feat: 860. Lemonade Change (CSHARP)

---
+++ csharp/860-lemonade-change/solution.cs
@@ -0,0 +1,45 @@
+public class Solution
+{
+    public bool LemonadeChange(int[] bills)
+    {
+        int fiveDollars = 0, tenDollars = 0;
+
+        foreach (int bill in bills)
+        {
+            if (bill == 5)
+            {
+                fiveDollars++;
+            }
+            else if (bill == 10)
+            {
+                if (fiveDollars > 0)
+                {
+                    fiveDollars--;
+                    tenDollars++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (fiveDollars > 0 && tenDollars > 0)
+                {
+                    fiveDollars--;
+                    tenDollars--;
+                }
+                else if (fiveDollars >= 3)
+                {
+                    fiveDollars -= 3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
